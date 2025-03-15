159f8d10e9241b7ff8246b9519a693c0cec6aa81
Emerson <the.emerson.araujo@gmail.com>
2025-03-15 17:49:46 -0300

    alteração

--- main.java
+++ main.java
@@ -1,5 +1,12 @@
 public class main {
 
+    public static void main(String[] args) {
+        int numero = 20;
+
+        String palavra = "Estudei java e fiquei calvo";
+        
+        
+    }
 }
 
 
