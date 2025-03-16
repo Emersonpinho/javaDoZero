b67875f4bc14096a765517bbc20845b8a9e889a3
Emerson <the.emerson.araujo@gmail.com>
2025-03-15 22:21:42 -0300

    explicações adicionais

--- main.java
+++ main.java
@@ -1,10 +1,13 @@
 public class main {
 
     public static void main(String[] args) {
+        
+        // altamente tipada
         int numero = 20;
 
         String palavra = "Estudei java e fiquei calvo";
 
+        // inferencia de tipo
         var n1 = 1;
     }
 }
